e288f98ded93f3b8f525087346a12ed9c9885160
juri-220 <lvshuning@gmail.com>
2021-06-21 18:29:09 -0500

    test

--- test_shuning.R
+++ test_shuning.R
@@ -1,6 +1,6 @@
 ###test###
 
-df<-read.table("https://s3.us-east-1.amazonaws.com/hdx-production-filestore/resources/7c8f6c93-6272-4d39-9e5d-99cdc0053dfc/2020-12-16_country_country.tsv?AWSAccessKeyId=AKIAXYC32WNARK756OUG&Expires=1624310148&Signature=CPDU7rBkHafhJE3aGwigId%2FmC4g%3D", head=T)
+df<-read.table("https://s3.us-east-1.amazonaws.com/hdx-production-filestore/resources/7c8f6c93-6272-4d39-9e5d-99cdc0053dfc/2020-12-16_country_country.tsv", head=T)
 
 
 
@@ -15,7 +15,7 @@ links<-df
 #get GDP per capita
 gdp = WDI(indicator='NY.GDP.PCAP.CD',start=2019, end=2019)
 
-#get internet per thousand people
+#get internet per thousand
 internet = WDI(indicator='IT.NET.USER.P3',start=2019, end=2019)
 
 
