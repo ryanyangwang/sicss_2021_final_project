7fa8944fd3f1efb55635b6d6b6d56cd65c6eddf5
juri-220 <lvshuning@gmail.com>
2021-06-21 18:37:13 -0500

    % of population

--- test_shuning.R
+++ test_shuning.R
@@ -3,8 +3,6 @@
 df<-read.table("https://s3.us-east-1.amazonaws.com/hdx-production-filestore/resources/7c8f6c93-6272-4d39-9e5d-99cdc0053dfc/2020-12-16_country_country.tsv", head=T)
 
 
-
-
 library("igraph") 
 library("statnet") 
 library("tidyverse")
@@ -15,8 +13,8 @@ links<-df
 #get GDP per capita
 gdp = WDI(indicator='NY.GDP.PCAP.CD',start=2019, end=2019)
 
-#get internet per thousand
-internet = WDI(indicator='IT.NET.USER.P3',start=2019, end=2019)
+#get "Individuals using the Internet (% of population)"   
+internet = WDI(indicator='IT.NET.USER.ZS',start=2019, end=2019)
 
 
 
