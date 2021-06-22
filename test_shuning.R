ef9c4e9db01724d2985a7f8bac280fe8eb0b4b45
juri-220 <lvshuning@gmail.com>
2021-06-21 19:48:44 -0500

    add press freedom

--- test_shuning.R
+++ test_shuning.R
@@ -23,3 +23,12 @@ polistable= WDI(indicator='PV.EST',start=2019, end=2019)
 
 #get outbound mobility ratio, Number of students from a given country studying abroad as a percentage of the total tertiary enrolment in that country. 
 outboundmob= WDI(indicator='UIS.OMR.56',start=2017, end=2020) 
+
+library("jsonlite")
+fsi <- fromJSON("https://gist.githubusercontent.com/planemad/76c9a87a4e2c785fb60081841c8c5d32/raw/d9e9d20c53f2dec82249b1f728c817ea39124a7d/world-press-freedom-index-2020.json",flatten=TRUE)
+colnames(fsi)
+
+#get freedom express 
+pressfree <-fsi[c('ISO2','EN_country','Score 2020')]
+pressfree$`Score 2020`<-gsub(",", ".", pressfree$`Score 2020`)
+pressfree$`Score 2020`<-as.numeric(pressfree$`Score 2020`)
