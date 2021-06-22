45bb74fdf1ac869781d9d3b871673e5020f86b6e
juri-220 <lvshuning@gmail.com>
2021-06-21 19:29:10 -0500

    update info on NA

--- test_shuning.R
+++ test_shuning.R
@@ -1,6 +1,7 @@
 ###        The file contains codes getting country-level features            ###
 
 #The dataframe includes iso2c ID for countries, which could be linked to FB SCI#
+#There are a lot of missing data for GINI, HDI, outbound mobility ratio so that more years should be included.#
 
 
 library("WDI")
@@ -8,8 +9,8 @@ library("WDI")
 #get GDP per capita
 gdp = WDI(indicator='NY.GDP.PCAP.CD',start=2019, end=2019)
 
-#get gini  "GINI index (World Bank estimate)"     
-gini=WDI(indicator='SI.POV.GINI',start=2019, end=2019) 
+#get gini  "GINI index (World Bank estimate)"
+gini=WDI(indicator='SI.POV.GINI',start=2017, end=2019) 
 
 #get "Human Development Index " 2019, 2020 not available 
 HDI = WDI(indicator='IDX.HDI.REV',start=2018, end=2018) 
@@ -21,4 +22,4 @@ internet = WDI(indicator='IT.NET.USER.ZS',start=2019, end=2019)
 polistable= WDI(indicator='PV.EST',start=2019, end=2019) 
 
 #get outbound mobility ratio, Number of students from a given country studying abroad as a percentage of the total tertiary enrolment in that country. 
-outboundmob= WDI(indicator='UIS.OMR.56',start=2019, end=2019) 
+outboundmob= WDI(indicator='UIS.OMR.56',start=2017, end=2020) 
